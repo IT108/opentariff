ca50c3899a00515cb20d2ecf30198a9f7dac4286
Dmitry Paramonov <gamiysikdima@gmail.com>
2020-10-26 20:26:19 +0300

    release version

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 opentariff
+# Envvars
+| variable                   | default       | description              |
+| ---------------------------|:-------------:| ------------------------:|
+| OT_PORT                    | 8081          | external port            |
+| OT_APPSEARCH_ENDPOINT      | 172.19.0.2    |appsearch ip in docker net|
+| SEARCH_API_KEY             | ...           |appsearch api key         |
+| SEARCH_ENGINE_NAME         | tariffs       |search engine name        |
+| OT_ELASTICSEARCH_ENDPOINT  | 172.19.0.5    |elastic internal ip       |
+
+
---
+++ app/views.py
@@ -0,0 +1,33 @@
+from flask import render_template, request
+from app.search import engine
+from app import app
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/find-tariff')
+def find_tariff():
+    return render_template('find-tariff.html')
+
+
+@app.route('/api/tariff_search', methods=['POST'])
+def search():
+    # print(request.form)
+    search_result = engine.search(request.form['query'])
+    return render_template('list_template.html', items=search_result['results'])
+
+
+@app.route('/api/get_tariff', methods=['POST'])
+def get_tariff():
+    search_result = engine.get_by_id(request.form['tariff_id'])
+    res = {'result': search_result}
+    print(res)
+    return res
